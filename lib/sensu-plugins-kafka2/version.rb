11e8b8aa7f5b2f04c14ae1753c52d8045f7af5e5
Mitsutoshi Aoe <me@maoe.name>
2020-01-29 11:32:49 +0900

    Bump version numbmer to 0.0..2.pre.tsuru0

--- lib/sensu-plugins-kafka2/version.rb
+++ lib/sensu-plugins-kafka2/version.rb
@@ -2,8 +2,8 @@ module SensuPluginsKafka2
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
+    PATCH = 2
 
-    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    VER_STRING = [MAJOR, MINOR, PATCH, "pre.tsuru0"].compact.join('.')
   end
 end
